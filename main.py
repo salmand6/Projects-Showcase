99976ad809750b2e7885e81a2df9e748db85bf2a
Salmand <igdeloyolasalmand@gmail.com>
2022-12-20 00:46:38 +0100

    Add Description on the new Columns

--- main.py
+++ main.py
@@ -9,11 +9,12 @@ with col1:
 with col2:
     st.title("Ignacio Salmerón Andreu")
     content = """
-    Hola soy un estudiante de doble grado trabajador, ambicioso y con ganas de aprender. Me gusta trabajar para resolver problemas y 
-    creo en el esfuerzo como la manera de alcanzar grandes objetivos en la vida. Aptitud para planificar, manejar la 
-    presión y liderar proyectos. Ganas de avanzar y destacar en mi carrera. Auto-didacta y apasionado de Python.\n
-    Ambitious and eager to learn Law and Business student. I like to work to solve problems. I believe in effort as the 
-    ultimate way of achieving big things in life. Apt to plan, handle pressure and lead the way. In love with Python.
+    Hola soy un estudiante de doble grado trabajador, ambicioso y con ganas de aprender. Me gusta trabajar para resolver 
+    problemas y creo en el esfuerzo como la manera de alcanzar grandes objetivos en la vida. Aptitud para planificar, 
+    manejar la presión y liderar proyectos. Auto-didacta y apasionado de Python.\n
+    Hi! I'm an ambitious and eager to learn Law and Business student. I like to work to solve problems. I believe in 
+    effort as the ultimate way of achieving big things in life. Apt to plan, handle pressure and lead the way. 
+    In love with Python.
     """
     st.info(content)
 
