53742f97ae6b68a9e11d17f5b81b126d79334f5a
Salmand <igdeloyolasalmand@gmail.com>
2022-12-22 23:22:56 +0100

    Add Navigation Menu and Contact Us page

--- Home.py
+++ Home.py
@@ -35,10 +35,10 @@ with col3:
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"(Source Code)({row['url']})")
+        st.write(f"[Source Code]({row['url']})")
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
-        st.write(f"(Source Code)({row['url']})")
+        st.write(f"[Source Code]({row['url']})")
